d49ad56f57a4ced3f2324e7edd0ceeac816be6c9
liuning <liuning@yiqifei.com>
2024-11-19 11:53:58 +0800

    修复 Applications.Parameters 在最新的 dify 下报错的问题

--- DifyAI/ObjectModels/SystemParametersOptions.cs
+++ DifyAI/ObjectModels/SystemParametersOptions.cs
@@ -8,6 +8,18 @@ namespace DifyAI.ObjectModels
         ///  图片文件上传大小限制（MB）
         /// </summary>
         [JsonPropertyName("image_file_size_limit")]
-        public string ImageFileSizeLimit { get; set; }
+        public int ImageFileSizeLimit { get; set; }
+        
+        [JsonPropertyName("video_file_size_limit")]
+        public int VideoFileSizeLimit { get; set; }
+        
+        [JsonPropertyName("audio_file_size_limit")]
+        public int AudioFileSizeLimit { get; set; }
+        
+        [JsonPropertyName("file_size_limit")]
+        public int FileSizeLimit { get; set; }
+        
+        [JsonPropertyName("workflow_file_upload_limit")]
+        public int WorkflowFileUploadLimit { get; set; }
     }
 }
